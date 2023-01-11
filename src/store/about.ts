71da3075ff5f825608575c4e7ca84c61ff255fc9
donghyeok26 <cdh93@naver.com>
2023-01-11 19:46:24 +0900

    Compiled Tpyescript

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,5 +1,14 @@
 import { Store } from "../core/hyeok";
 
+interface State{
+    photo: string
+    name: string
+    email: string
+    blog: string
+    github: string
+    repository: string
+}
+
 export default new Store({
     photo: 'https://heropy.blog/css/images/logo.png',
     name: 'HEROPY / ParkYoungWoong',
